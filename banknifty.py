ee7a43edc4380c815f4dc0efb65d755064f54910
zahidshaikmohammed-cmyk <zahidshaikmohammed@gmail.com>
2026-09-12 17:18:44 +0530

    Add dedicated BankNifty endpoint

---
+++ banknifty.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from datetime import datetime
+from types import SimpleNamespace
+from zoneinfo import ZoneInfo
+
+from dhanhq import DhanContext, MarketFeed
+
+from banknifty import *  # pragma: no cover
